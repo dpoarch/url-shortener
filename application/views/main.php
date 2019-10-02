bdeb281bc138b0c81e2ad5c84a297847ebc5da1f
David Poarch <davidpoarch@outlook.com>
2019-10-02 06:02:23 -0400

    added jquery ajax posting of data

--- application/views/main.php
+++ application/views/main.php
@@ -75,15 +75,44 @@
 
 
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
     <script>
 
         $(document).on('click', '#btn_submit', function(){
-            alert('heelo');
+
+            send_data();
         });
+
+        function send_data(){
+
+            $.ajax({
+  type: 'POST',
+  url: '/index.php/url/create',
+  data: {url: 'aaaaa'},
+  dataType: 'json',
+  success: function(data){console.log(data)}
+});
+
+/*
+            $.ajax({
+                        type      : 'POST',
+                        url       : '/index.php/url/create',
+                        data      :  {url : $('#txt_url').val() },
+                        dataType  : 'json',
+                        success   : function(data) {
+                                        console.log(data);
+                                    }
+                    });
+*/
+
+        }
+
+        function load_data(){
+
+        }
 
     </script>
   </body>
